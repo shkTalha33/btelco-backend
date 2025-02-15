e2fb2443729bd06e6c03ed40773eb6b94b17481c
shkTalha33 <shykhtalha33@gmail.com>
2025-02-15 22:11:13 +0500

    changes

--- index.js
+++ index.js
@@ -41,6 +41,5 @@ app.use("/api", pageRoutes);
 
 app.use("./public/images", express.static(path.join(__dirname, "public/images")));
 
-// Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
