4cf301afc2da5f6409b0c26f5a4dc3872ff092fe
shkTalha33 <shykhtalha33@gmail.com>
2025-02-16 14:18:21 +0500

    static folder added

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.use(
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.static("public"));
 
 // Routes
 app.use("/api", quoteRoutes);
