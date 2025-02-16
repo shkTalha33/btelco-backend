ceebf742b6f9822356e8693b196a00c7698008d1
shkTalha33 <shykhtalha33@gmail.com>
2025-02-16 17:26:44 +0500

    update static file serving and multer storage destination; add .gitkeep to public directory

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ app.use(
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
-app.use(express.static(path.join(__dirname, "/uploads")));
+app.use(express.static("public"));
+
 
 // Routes
 app.use("/api", quoteRoutes);
--- middleware/multer.js
+++ middleware/multer.js
@@ -2,10 +2,11 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads/");
+    cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname); // not good approach to use .originalname can be use .filename
   },
 });
 
